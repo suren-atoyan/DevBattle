26505b16ae78e311977dec846c33b4ac2b9271e6
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-02 01:41:34 +0400

    FrontEnd ::: Create empty ReactJs application by CRA (create-react-app@next)

---
+++ public/hackathon/src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './App.scss';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="app">
+        Hackathon!!!
+      </div>
+    );
+  }
+}
+
+export default App;
