32e4dd09e59f65feb8a4ef1de0f5c0d4a454994b
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-02 20:27:43 +0400

    Server ::: Return 'index.html' in any case

--- routes/main/index.js
+++ routes/main/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/hackathon/build/index.html'));
 });
 
