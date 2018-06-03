544ac5125396fa67e68baa8b8226eb955bf18453
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-04 00:05:22 +0400

    FrontEnd ::: Add util - omit

--- public/hackathon/src/utils/index.js
+++ public/hackathon/src/utils/index.js
@@ -4,4 +4,9 @@ const getRouteTitle = location => {
   return location === '/' ? existingRoutes.default : location.split('/')[1].toUpperCase();
 }
 
-export { getRouteTitle };
+const omit = (obj, exclude) =>
+  Object.keys(obj).reduce((acc, item) =>
+    // eslint-disable-next-line
+    (!exclude.includes(item) && (acc[item] = obj[item]), acc), {});
+
+export { getRouteTitle, omit };
