d4eaaafd8bacd6d73fb46c5865aa33e9a87056cd
Tigran <matsakyan.7@gmail.com>
2018-06-04 00:40:36 +0400

    Server ::: Integrate check-token middleware

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
+const exclude = require('../middlewares/exclude-express-route');
+const config  = require('../config');
+
 module.exports = function(app) {
 
-  // API
-  app.use('/api/v1.0.0/', require('./api'));
+  // API;
 
-  app.use('/', require('./main'));
+  app.use(
+    '/api/v1.0.0/',
+    exclude(config.get('exclude_from_auth'), require('../middlewares/check-token')),
+    require('./api')
+  );
+
+  // TODO ::: Check is it necessary to exlude /api?
+  app.use('/', exclude('/api', require('./main')));
 }
