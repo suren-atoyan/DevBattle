4ea84f7d92f44677f99d56035878c9e67b84b741
Tigran <matsakyan.7@gmail.com>
2018-06-04 00:42:37 +0400

    Server ::: Create check-token and exclude-exress-route middlewares

---
+++ middlewares/exclude-express-route.js
@@ -0,0 +1,24 @@
+/**
+ * exclude-express-route middleware to exclude some url-s from express route
+ * @module middleware
+ * @version 1.0.1
+ */
+
+/**
+ * @const exclude
+ * @param {Object} exclude - excluded path
+ * @param {function} middleware - middleware function
+ * @returns {function} next | middleware
+ */
+const exclude = (exclude, middleware) => {
+
+  return (req, res, next) => {
+    if (exclude.some(item => req.path.includes(item))) {
+      return next();
+    }
+
+    return middleware(req, res, next);
+  }
+}
+
+module.exports = exclude;
