df754a802acc607d256d67e6d980129ad9f4cff7
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-04 11:10:48 +0400

    FrontEnd ::: Move out static properties from f.component

--- public/hackathon/src/auth/AuthMessage.js
+++ public/hackathon/src/auth/AuthMessage.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Snackbar from '@material-ui/core/Snackbar';
 
+// TODO ::: Add messages for all kind of statuses
+
+const getMessageBasedOnStatus = status => status === 401
+  ? 'You are not authorized'
+  : 'Something went wrong';
+
+const defaultSnackbarPosition = { vertical: 'top', horizontal: 'right' };
+
 const AuthMessage = props => {
-
-  // TODO ::: Add messages for all kind of statuses
-
-  const message = props.authData.status === 401
-    ? 'You are not authorized'
-    : 'Something went wrong';
+  const message = getMessageBasedOnStatus(props.authData.status);
 
   return (
     <Snackbar
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      anchorOrigin={defaultSnackbarPosition}
       message={message}
       open={true}
       onClose={props.handleClose}
