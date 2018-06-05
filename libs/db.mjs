18b4d15c1dc3e6400d38bd28eeca1549c3eceadb
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-05 12:31:22 +0400

    Server ::: Install lowDB and create lib for working with lowDB

---
+++ libs/db.mjs
@@ -0,0 +1,62 @@
+import lowDB from 'lowdb';
+
+import FileAsync from 'lowdb/adapters/FileAsync';
+
+import config from '../config';
+
+class DB {
+
+  constructor({ lowDB, FileAsync, config }) {
+    this.lowDB = lowDB;
+    this.FileAsync = FileAsync;
+    this.config = config;
+  }
+
+  async connect() {
+    const adapter = new FileAsync(config.get('db_path'));
+
+    this.db = await lowDB(adapter);
+
+    await this.setDefaults();
+
+    return this.db;
+  }
+
+  async setDefaults() {
+    return await this.db
+                     .defaults(config.get('default_data'))
+                     .write();
+  }
+
+  async get(key, value) {
+    return await this.db
+                     .get(key)
+                     .value();
+  }
+
+  async setPush(key, value) {
+    return await this.db
+                     .get(key)
+                     .push(value)
+                     .write();
+  }
+
+  async set(...args) {
+    return await this.db
+                     .set(...args)
+                     .write();
+  }
+
+  async update(...args) {
+    return await this.db
+               .update(...args)
+               .write()
+  }
+
+}
+
+export default new DB({
+  lowDB,
+  FileAsync,
+  config,
+});
