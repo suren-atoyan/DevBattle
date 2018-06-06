d38566361fb4f4e0339896c7e9df695e0acc517a
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-07 01:35:49 +0400

    FrontEnd ::: Implement monaco with react in Challenges page

--- public/hackathon/src/pages/Challenges/index.js
+++ public/hackathon/src/pages/Challenges/index.js
@@ -1,10 +1,59 @@
 import React, { PureComponent } from 'react';
 
+import Button from '@material-ui/core/Button';
+
+import Editor from 'components/Editor';
+
+// Example
+
+const sourceExample = `class Auth {
+  static genRandomCryptoString(len) {
+    return crypto.randomBytes(Math.ceil(len / 2)).toString('hex').slice(0, len);
+  }
+
+  // ...
+}
+`;
+
 export default class Challenges extends PureComponent {
+
+  state = {
+    isEditorMounted: false,
+    theme: 'vs-dark',
+  }
+
+  editorDidMount = getEditorValue => {
+    this.getEditorValue = getEditorValue;
+
+    this.setState({ isEditorMounted: true });
+  }
+
+  sendResult = _ => {
+    // ...
+  }
+
+  toggleTheme = _ => this.setState({ theme: this.state.theme.endsWith('dark') ? 'vs' : 'vs-dark' });
+
   render() {
     return (
       <div>
-        Challenges
+        <Editor
+          value={sourceExample}
+          valueGetter={getEditorValue => (this.getEditorValue = getEditorValue)}
+          editorDidMount={this.editorDidMount}
+          theme={this.state.theme}
+        />
+        <Button
+          onClick={this.sendResult}
+          disabled={this.isEditorMounted}
+        >
+          Submit
+        </Button>
+        <Button
+          onClick={this.toggleTheme}
+        >
+          Toggle Theme
+        </Button>
       </div>
     );
   }
