0c3fab7b609f41dd23b5fc21b424e6fb68f213eb
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-07 15:54:11 +0400

    FrontEnd :: Create logic for showing different contents with different styles if there is or no active hackathons and challenges

--- public/hackathon/src/pages/Challenges/index.js
+++ public/hackathon/src/pages/Challenges/index.js
@@ -1,19 +1,46 @@
-import React, { PureComponent } from 'react';
-
+import React, { PureComponent, Fragment } from 'react';
 import Challenge from './Challenge/';
-
 import Stepper from 'components/Stepper';
+import Typography from '@material-ui/core/Typography';
+import classNames from 'classnames';
+
+import { withStore } from 'store';
 
 import './index.scss';
 
-export default class Challenges extends PureComponent {
+class Challenges extends PureComponent {
 
   render() {
+
+    const { store: { activeHackathon } } = this.props;
+
+    const hasActiveHackathon = !!activeHackathon;
+
+    const hasChallenges = hasActiveHackathon && activeHackathon.challenges.length;
+
     return (
-      <div className="challenges__wrapper">
-        <div className="challenge__wrapper"><Challenge /></div>
-        <div className="challenges__stepper"><Stepper /></div>
+      <div
+        className={classNames('challenges__wrapper', {
+          centered: !hasChallenges,
+        })}
+      >
+        {
+          hasActiveHackathon || hasChallenges
+            ? (
+                <Fragment>
+                  <div className="challenge__wrapper"><Challenge /></div>
+                  <div className="challenges__stepper"><Stepper /></div>
+                </Fragment>
+              )
+            : (
+                <Typography variant="display3" align="center" className="challenges__noch">
+                  {`There is no active ${!hasActiveHackathon ? 'hackathons' : 'challenges'}`}
+                </Typography>
+              )
+        }
       </div>
     );
   }
 }
+
+export default withStore(Challenges);
