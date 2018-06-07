3d6dd171bdc8fd5e1febeffd9f011f223639864e
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-07 14:11:07 +0400

    FrontEnd ::: Create Stepper component (draft)

---
+++ public/hackathon/src/components/Stepper/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+    position: 'absolute',
+  },
+  dot: {
+    marginLeft: '20px'
+  }
+};
+
+class DotsMobileStepper extends React.Component {
+  state = {
+    activeStep: 0,
+  };
+
+  handleNext = () => {
+    this.setState({
+      activeStep: this.state.activeStep + 1,
+    });
+  };
+
+  handleBack = () => {
+    this.setState({
+      activeStep: this.state.activeStep - 1,
+    });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+
+    return (
+      <MobileStepper
+        variant="dots"
+        steps={10}
+        position="bottom"
+        activeStep={this.state.activeStep}
+        classes={classes}
+        nextButton={
+          <Button size="small" onClick={this.handleNext} disabled={this.state.activeStep === 5}>
+            Next
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={this.handleBack} disabled={this.state.activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+          </Button>
+        }
+      />
+    );
+  }
+}
+
+DotsMobileStepper.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(DotsMobileStepper);
