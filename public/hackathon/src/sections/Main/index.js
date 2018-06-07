d9ea340a1379e053db5c9380038fac6e535c72d4
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-07 14:10:30 +0400

    FrontEnd ::: Wrap up main content with Paper

--- public/hackathon/src/sections/Main/index.js
+++ public/hackathon/src/sections/Main/index.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { Admin, Monitoring, Challenges } from 'pages';
 import NoMatch from 'components/NoMatch';
+import Paper from '@material-ui/core/Paper';
 import PrivateRoute from 'auth/PrivateRoute';
 import { withAuth } from 'auth';
 
+import './index.scss';
+
 const Content = ({ match, authState: { isAdmin, isGuest, isTeamMember } }) => {
   return (
-    <Switch>
-      <Route exact path="/" component={Monitoring} />
-      <Route path="/monitoring" component={Monitoring} />
-      <PrivateRoute path="/challenges" hasAccess={isAdmin || isGuest || isTeamMember} component={Challenges} />
-      <PrivateRoute path="/admin" hasAccess={isAdmin} component={Admin} />
-      <Route exact path="/404" component={NoMatch} />
-      <Redirect to="/404" />
-    </Switch>
+    <div className="main__wrapper">
+      <Paper className="main__wrapper--paper" elevation={8}>
+        <Switch>
+          <Route exact path="/" component={Monitoring} />
+          <Route path="/monitoring" component={Monitoring} />
+          <PrivateRoute path="/challenges" hasAccess={isAdmin || isGuest || isTeamMember} component={Challenges} />
+          <PrivateRoute path="/admin" hasAccess={isAdmin} component={Admin} />
+          <Route exact path="/404" component={NoMatch} />
+          <Redirect to="/404" />
+        </Switch>
+      </Paper>
+    </div>
   );
 }
 
