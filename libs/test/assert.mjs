7e807aa9de64d850e68a666a0ae21584be37031b
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-10 02:49:24 +0400

    Server ::: Add TODO in assert module

--- libs/test/assert.mjs
+++ libs/test/assert.mjs
@@ -1,7 +1,9 @@
 export default (input, output, fn) => {
-    try {
-        return JSON.stringify(fn(...input)) === JSON.stringify(output);
-    } catch {
-        return false;
-    }
+  try {
+    return JSON.stringify(fn(...input)) === JSON.stringify(output);
+  } catch(err) {
+    console.log(err)
+    // TODO ::: Catch error message for sending users
+    return false;
+  }
 }
