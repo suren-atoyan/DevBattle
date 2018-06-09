3b8a92a6f9742924421d36b1fba592381660a2c6
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-09 18:57:08 +0400

    Server ::: Add new route for submit challenge answer (demo)

---
+++ routes/api/challenge-answer.mjs
@@ -0,0 +1,22 @@
+import auth from '../../libs/auth';
+import { asyncWrapper } from '../../libs/utils';
+
+async function _challengeAnswer(req, res) {
+  const { cookies : { token }, body } = req;
+  const role = await auth.getRoleByToken(token);
+
+  if (role) {
+    if (body) {
+      const result = { id: body.id, correct: true, errorMessage: null };
+      res.status(200).send(result);
+    } else {
+      res.status(422).send({});
+    }
+  } else {
+    res.status(401).send({ message: 'Authentication failed.' });
+  }
+}
+
+const challengeAnswer = asyncWrapper(_challengeAnswer);
+
+export default challengeAnswer;
