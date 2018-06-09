937987f059394b95118067db5d6eb6cb61d52899
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-09 18:54:31 +0400

    Server ::: Fix name convention issue

--- routes/api/check-token.mjs
+++ routes/api/check-token.mjs
@@ -3,9 +3,10 @@ import { asyncWrapper } from '../../libs/utils';
 
 const checkToken = async (req, res) => {
   const { token } = req.cookies;
-  const { pass, exp, iat, ...rest } = await auth.verify(token);
 
-  res.send(rest.role || {});
+  const { pass, exp, iat, ...role } = await auth.verify(token);
+
+  res.send(role || {});
 }
 
 export default asyncWrapper(checkToken);
