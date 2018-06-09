e5a796745472b2ea79f55128ee3c6153e0789bb1
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-10 02:52:07 +0400

    Server ::: Create sample for testing testRunner functionality

--- routes/api/challenge-answer.mjs
+++ routes/api/challenge-answer.mjs
@@ -1,17 +1,48 @@
 import auth from '../../libs/auth';
 import { asyncWrapper } from '../../libs/utils';
 
+import testRunner from '../../libs/test';
+
 async function _challengeAnswer(req, res) {
-  const { cookies : { token }, body } = req;
+  const { cookies : { token }, body: { challengeId, source } } = req;
   const role = await auth.getRoleByToken(token);
 
   if (role) {
-    if (body) {
-      const result = { id: body.id, correct: true, errorMessage: null };
-      res.status(200).send(result);
-    } else {
-      res.status(422).send({});
+
+    // Just Sample
+
+    const requirements = {
+      tests: [
+        {
+          input: [2],
+          output: 4,
+        },
+        {
+          input: [4],
+          output: 16,
+        },
+        {
+          input: [8],
+          output: 64,
+        },
+      ],
+
+      name: 'test',
+    };
+
+    // find challenge by challengeId from body
+    // and get challenge requirements
+    // execute source and check it here
+
+    // TODO ::: Make testRunner function execution asynchronous.
+
+    const result = testRunner(requirements, source);
+
+    if (!role.isAdmin && (role.isGuest || role.isTeamMember)) {
+      // TODO ::: Write logic for saving result in proper part of data base
     }
+
+    res.status(200).send({ success: result }); // true/false will be calculated from line 8
   } else {
     res.status(401).send({ errorMessage: 'Authentication failed.' });
   }
