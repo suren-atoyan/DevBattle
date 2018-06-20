438969672971908845850b6fce8e6d15b08ce19f
Armen Nikoyan <a.nikoyan@apollobytes.com>
2018-06-20 22:46:55 +0400

    Server ::: Add Get results route

--- routes/api/index.mjs
+++ routes/api/index.mjs
@@ -4,6 +4,7 @@ import login from './login';
 import logout from './logout';
 import checkToken from './check-token';
 import { createTeam } from './team';
+import { getResults } from './results';
 import {
   createHackathon,
   getHackathon,
@@ -22,6 +23,7 @@ router.post('/hackathons/start', startHackathon);
 router.post('/hackathons/finish', finishHackathon);
 router.post('/challenge_answer', challengeAnswer);
 router.post('/team', createTeam);
+router.get('/results', getResults);
 
 router.get('/check_token', checkToken);
 
