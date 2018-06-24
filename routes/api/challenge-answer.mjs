bfc8b53967e0a7b7ed8fb7a20d6dc689991f55f3
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-24 21:45:49 +0400

    Server ::: Fix spelling mistake

--- routes/api/challenge-answer.mjs
+++ routes/api/challenge-answer.mjs
@@ -75,7 +75,7 @@ async function challengeAnswer(req, res) {
   if (existingSolution) {
 
     if (!currnetChallenge.points || !currnetChallenge.fnLength) return handleInvalidRequest(res, 400, 'already_solved');
-    if (result.points <= existingSolution.points) return handleInvalidRequest(res, 400, 'better_sollution');
+    if (result.points <= existingSolution.points) return handleInvalidRequest(res, 400, 'better_solution');
 
     // Update challenge points
     currentTeamResults.score += result.points - existingSolution.points;
