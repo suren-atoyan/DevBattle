1dbffc1b3862ef83a944be71a298320bbaf80641
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-26 01:29:44 +0400

    Complete helper texts and move to config

--- public/hackathon/src/config/index.js
+++ public/hackathon/src/config/index.js
@@ -39,10 +39,18 @@ const consoleWarnTextStyles = `
   text-transform: uppercase;
 `;
 
+const helperTexts = {
+  fnName: 'It is the name of the function, which will be called after the execution of your code, and to which the test parameters will be passed. You can write additional variables or functions too, but the function with this very name will be called.',
+  sourceLength: 'This is the maximum length of the source, that can be used for passing the challenge. Pay attention that challenges can also be passed by less number of characters than is mentioned here and you will get additional points for that. More in Points section.',
+  exclude: 'Those are symbols/fragments that you can\'t use in your source.',
+  points: 'These are the points that are given for the challenge. If the challenge has a source limitation, it means that when you satisfy to the specified limitations you get the points mentioned here and if you write the code shorter you can get bonus with the size of the points mentioned here.',
+}
+
 export {
   existingRoutes,
   consoleWarnDefaultText,
   consoleWarnTextStyles,
+  helperTexts,
   url,
   maximum_allowed_code_length,
   REACT_APP_LOCAL_SERVER_URL as localServerUrl,
