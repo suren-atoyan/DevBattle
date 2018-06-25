f872a107db3815a9900a1d22a7a40fd73a9d4434
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-25 15:32:54 +0400

    Server ::: Implement uws process error handling, add TODOs

--- ws/index.mjs
+++ ws/index.mjs
@@ -10,11 +10,20 @@ const uwsServerPath = path.resolve(__dirname, './uws-server.mjs');
 
 const parameters = config.get('uws_server').child_process_parameters;
 const options = {
-  stdio: [0, 1, 2, 'ipc'], // "0, 1, 2" -> mean "process.stdin, process.stdout, process.stderr"
+  stdio: [0, 1, 'pipe', 'ipc'], // "0, 1, 2" -> mean "process.stdin, process.stdout, process.stderr"
   // And 'ipc' for creating an Inter Process Communication (IPC) channel
   // for passing messages/file descriptors between parent and child.
 };
 
 const uwsServer = fork(uwsServerPath, parameters, options);
+
+uwsServer.stderr.on('data', errorBuffer => {
+  const error = new Error(errorBuffer);
+
+  // Handle uWs server errors here.
+  // TODO ::: Check all types of errors
+  // And do proper actions depending on error type
+  // Implement uws process re-spawn
+});
 
 export default uwsServer;
