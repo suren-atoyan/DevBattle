60b166aa4b12fa076e09c63b840b314ba9268111
Suren Atoyan <surenatoyan93@gmail.com>
2018-06-29 11:49:36 +0400

    Server ::: Change team name/password length

--- models/team.mjs
+++ models/team.mjs
@@ -4,8 +4,8 @@ import uuid from 'uuid/v1';
 import Joi from 'joi';
 
 const teamSchema = {
-  name: Joi.string().min(4).max(12).required(),
-  password: Joi.string().min(4).max(12).required(),
+  name: Joi.string().min(3).max(20).required(),
+  password: Joi.string().min(4).max(20).required(),
 
   count: Joi.number().empty().default(10),
   _id: Joi.any().forbidden().default(_ => uuid(), 'unique id'),
